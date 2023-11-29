9eb497adc0f6bb8d0d489204e39aaf19a7de49c6
Niklas Hietala <niklas.hietala1997@gmail.com>
2023-11-29 17:07:46 +0200

    Added regexp validating for number field7

--- models/person.js
+++ models/person.js
@@ -18,11 +18,17 @@ mongoose
 const numberSchema = new mongoose.Schema({
   name: {
     type: String,
+    minlength: 3,
     required: true,
   },
   number: {
     type: String,
     minlength: 8,
+    validate: {
+        validator: (v) => {
+            return /^\d{2,3}-\d{6,}$/.test(v)
+        }
+    },
     required: true,
   },
   
